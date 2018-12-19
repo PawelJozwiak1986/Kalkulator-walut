823d30f0c6b284a03a614193895e57c483c8aee6
Pawel <paweljozwiak1986@gmail.com>
2018-12-19 09:40:48 +0100

    init

---
+++ styles.js
@@ -0,0 +1,27 @@
+let valueInput = document.querySelector(".initial__value");
+let outputCurrency = document.querySelector(".outputCurrency").value;
+let calkbtn = document.querySelector(".calkbtn");
+
+
+
+function calculateCurrency() {
+  
+  const init = valueInput.value;
+  if (outputCurrency = document.querySelector(".outputCurrency").value === 'USD'){
+    let wynik = init * 3.7668;
+    document.querySelector(".initial__value").value = wynik;
+
+  }
+    if(outputCurrency = document.querySelector(".outputCurrency").value === 'EUR'){
+      let wynik = init * 4.2853;
+      document.querySelector(".initial__value").value = wynik;
+      
+  }
+};
+ 
+
+calkbtn.addEventListener("click", calculateCurrency );
+
+
+
+
